c42a7b2f173425e65552141dddc8f5252a1f1fa3
rieg-ec <rieg@uc.cl>
2023-09-11 13:59:11 -0300

    chore: update playground

--- playground/electron/renderer.js
+++ playground/electron/renderer.js
@@ -1,3 +1,5 @@
 const Astrolytics = require('astrolytics-desktop');
 
-Astrolytics.init('sdfds');
+Astrolytics.init('64ff453ad47692f4e15d0416', { debug: true });
+
+Astrolytics.track('test', { test: 'test' });
